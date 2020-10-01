70b353d423eac7d663212cf50a21fdb3617a13f6
Arthur Stepaniuk <artur.stepaniuk@perfectial.com>
2020-10-01 18:31:54 +0300

    migrated project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # size-reporter
 Gets approximate size of json fed to it
+
+Throw jsons from your blob in results folder and 
+
+`node app.js`
---
+++ app.js
@@ -0,0 +1,51 @@
+// const sizeof = require("sizeof");
+const sizeof = require("object-sizeof");
+const fs = require("fs").promises;
+const {
+  errorPath,
+  sizeResults,
+  formatBytes,
+  sortObjByValues,
+} = require("./helpers");
+
+const prepareResults = async (resultObject, filename) => {
+  let holder = {};
+  let errors = [];
+  let totalSize = formatBytes(sizeof(resultObject));
+
+  const updateHolder = (obj, key) => {
+    const size = sizeof(obj[key]);
+    holder = { ...holder, [key]: size };
+  };
+
+  const getValuesSize = (obj) => {
+    try {
+      const keys = Object.keys(obj);
+      for (let key of keys) {
+        updateHolder(obj, key);
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+          getValuesSize(obj[key]);
+        }
+        if (Array.isArray(obj[key])) {
+          updateHolder(obj, key);
+        }
+      }
+    } catch (err) {
+      errors.push(err);
+    }
+  };
+
+  getValuesSize(resultObject);
+  await fs.writeFile(
+    sizeResults(filename),
+    JSON.stringify(sortObjByValues({...holder, totalSize}), null, 4)
+  );
+  await fs.writeFile(errorPath(filename), JSON.stringify(errors, null, 4));
+};
+
+(async () => {
+  const filenames = await fs.readdir("./results");
+  for await (filename of filenames) {
+    await prepareResults(require(`./results/${filename}`), filename);
+  }
+})();
